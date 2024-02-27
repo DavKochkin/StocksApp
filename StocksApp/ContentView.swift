9a1f30cb902879411eeb45bbcb2bc6e4d55adc51
David Kochkin <kochkin.david@gmail.com>
2024-02-27 22:19:03 +0700

    Setup

--- StocksApp/ContentView.swift
+++ StocksApp/ContentView.swift
@@ -14,7 +14,25 @@ struct ContentView: View {
         NavigationStack {
             List {
                 ForEach (0...10, id: \.self) { number in
-                    Text("\(number)")
+                    HStack {
+                        Text("Symbol")
+                        
+                        Spacer()
+                        
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(width: 150, height: 50)
+                        
+                        VStack(alignment: .trailing) {
+                            Text("Value")
+                            Text("Change")
+                        }
+                    }
+                }
+            }
+            .navigationTitle("My Stocks")
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    EditButton()
                 }
             }
         }
